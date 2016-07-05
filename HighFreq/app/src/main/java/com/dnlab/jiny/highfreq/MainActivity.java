327641aa45f72ae2e56bc84a166735299638082e
HyungJiny <bbqnn200@gmail.com>
2016-07-05 22:32:40 +0900

    사운드 추가

--- HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
+++ HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
@@ -1,13 +1,22 @@
 package com.dnlab.jiny.highfreq;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    SoundPool highFrequenceSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        highFrequenceSound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        int sound167khz = highFrequenceSound.load(this, R.raw.167khz, 1);
+        
     }
+
+
 }
