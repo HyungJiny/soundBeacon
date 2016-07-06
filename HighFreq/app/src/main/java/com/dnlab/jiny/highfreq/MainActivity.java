b42cf0f723da20f7f0cbc1a0321b92df24e7df61
HyungJiny <bbqnn200@gmail.com>
2016-07-06 09:55:52 +0900

    add sound play and stop button

--- HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
+++ HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
@@ -4,9 +4,13 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     SoundPool highFrequenceSound;
+    Button playButton;
+    Button stopButton;
     private boolean isplay = false;
 
     @Override
@@ -15,7 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         highFrequenceSound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        int sound167khz = highFrequenceSound.load(this, R.raw.freq167sound, 1);
+        final int sound167khz = highFrequenceSound.load(this, R.raw.freq167sound, 1);
+
+        playButton = (Button) findViewById(R.id.play_button);
+        stopButton = (Button) findViewById(R.id.stop_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                highFrequenceSound.play(sound167khz, 1f, 1f, 0, 0 , 1f);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                highFrequenceSound.pause(sound167khz);
+            }
+        });
+
         
     }
 
