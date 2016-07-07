5d49906795285bdecdbf31bcfd5546191778ed41
HyungJiny <bbqnn200@gmail.com>
2016-07-07 17:26:12 +0900

    17khz까지 thread를 통해 출력 가능

--- HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
+++ HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
@@ -1,53 +1,74 @@
 package com.dnlab.jiny.highfreq;
 
+import android.media.AudioFormat;
 import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.AudioTrack;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    SoundPool highFrequenceSound;
-    Button playButton;
-    Button stopButton;
+
+    private final int duration = 10; // second
+    private final int sampleRate = 8000;
+    private final int numberOfSamples = duration * sampleRate;
+    private final double sample[] = new double[numberOfSamples];
+    private final double frequencyOfTune = 180000; // hz
+
+    private final byte generatedSound[] = new byte[2*numberOfSamples];
+
+    Handler handler = new Handler();
+
     private boolean isplay = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        highFrequenceSound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        final int sound167khz = highFrequenceSound.load(this, R.raw.freq167sound, 1);
+    @Override
+    protected void onResume(){
+        super.onResume();
 
-        playButton = (Button) findViewById(R.id.play_button);
-        stopButton = (Button) findViewById(R.id.stop_button);
-
-        playButton.setOnClickListener(new View.OnClickListener() {
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                highFrequenceSound.play(sound167khz, 1f, 1f, 0, 0 , 1f);
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        playSound();
+                    }
+                });
             }
         });
 
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                highFrequenceSound.pause(sound167khz);
-            }
-        });
-
-        
+        thread.start();
     }
 
-    private void playSound(){
-        //soundpool.play(soundload, leftVolume, rightVolume, priority, loop, frequency);
+    public boolean playSound(){
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, numberOfSamples,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSound, 0, numberOfSamples);
+        audioTrack.play();
+        return isplay = true;
     }
 
-    private void stopSound(){
+    public void genTone(){
+        for(int i=0; i<numberOfSamples; i++){
+            sample[i] = Math.sin(2*Math.PI * i / (sampleRate/frequencyOfTune));
+        }
 
+        int index = 0;
+        for(double dVal: sample){
+            short value = (short) (dVal*32767); // 32767=0x7fff
+            generatedSound[index++] = (byte) (value & 0x00ff);
+            generatedSound[index++] = (byte) ((value & 0xff00)>>8);
+        }
     }
-
-
 }
