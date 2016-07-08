5784016fdd8245bc797516d76d933917c8e0db1b
HyungJiny <bbqnn200@gmail.com>
2016-07-08 12:52:48 +0900

    add seek bar

--- HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
+++ HighFreq/app/src/main/java/com/dnlab/jiny/highfreq/MainActivity.java
@@ -8,50 +8,79 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int duration = 10; // second
+    private final int duration = 3; // second
     private final int sampleRate = 8000;
     private final int numberOfSamples = duration * sampleRate;
     private final double sample[] = new double[numberOfSamples];
-    private final double frequencyOfTune = 180000; // hz
+    private double frequencyOfTune = 440; // hz
 
     private final byte generatedSound[] = new byte[2*numberOfSamples];
-
     Handler handler = new Handler();
 
     private boolean isplay = false;
+
+    Button soundPlayButton;
+    SeekBar frequencySeekBar;
+    TextView frequencyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-
-        Thread thread = new Thread(new Runnable() {
+        frequencySeekBar = (SeekBar) findViewById(R.id.frequency_seekBar);
+        frequencyValue = (TextView) findViewById(R.id.frequency_value_textView);
+        frequencySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void run() {
-                genTone();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        playSound();
-                    }
-                });
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                if(progress < 20){
+                    progress = 20;
+                    seekBar.setProgress(progress);
+                }
+                frequencyValue.setText("현재 주파수 : " + progress + "Hz");
+                frequencyOfTune = (double) progress*10;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
-        thread.start();
+        soundPlayButton = (Button) findViewById(R.id.play_button);
+        soundPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        genTone();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                playSound();
+                            }
+                        });
+                    }
+                });
+                thread.start();
+            }
+        });
     }
 
     public boolean playSound(){
         AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
                 AudioFormat.ENCODING_PCM_16BIT, numberOfSamples,
                 AudioTrack.MODE_STATIC);
         audioTrack.write(generatedSound, 0, numberOfSamples);
@@ -61,14 +90,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void genTone(){
         for(int i=0; i<numberOfSamples; i++){
-            sample[i] = Math.sin(2*Math.PI * i / (sampleRate/frequencyOfTune));
+            sample[i] = Math.sin((2*Math.PI - 0.001)*i / (sampleRate/frequencyOfTune));
         }
 
         int index = 0;
-        for(double dVal: sample){
-            short value = (short) (dVal*32767); // 32767=0x7fff
-            generatedSound[index++] = (byte) (value & 0x00ff);
-            generatedSound[index++] = (byte) ((value & 0xff00)>>8);
+        int ramp = numberOfSamples / 20;
+
+        for(int i = 0; i< ramp; i++){
+            final short val = (short) ((sample[i] * 32767) * i / ramp);
+            generatedSound[index++] = (byte) (val & 0x00ff);
+            generatedSound[index++] = (byte) ((val & 0xff00)>>>8);
+        }
+
+        for(int i = ramp; i< numberOfSamples - ramp; i++){
+            final short val = (short) ((sample[i] * 32767));
+            generatedSound[index++] = (byte) (val & 0x00ff);
+            generatedSound[index++] = (byte) ((val & 0xff00)>>>8);
+        }
+
+        for(int i = numberOfSamples - ramp; i< numberOfSamples; i++){
+            final short val = (short) ((sample[i] * 32767) * (numberOfSamples - i) / ramp);
+            generatedSound[index++] = (byte) (val & 0x00ff);
+            generatedSound[index++] = (byte) ((val & 0xff00)>>>8);
         }
     }
 }
